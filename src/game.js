bd7d81d0cd74199fcbbd9489e71f3026d6afb5d2
Edmund Quintero <equintero@tradeking.com>
2016-11-11 19:22:49 -0500

    Adds game.js for Phaser bootstrap

---
+++ src/game.js
@@ -0,0 +1,31 @@
+/**
+ * Bootstraps the game and execute Phaser lifecycle hooks
+ */
+export const game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, '', { preload, create, update });
+
+/**
+ * preload
+ */
+function preload () {
+  game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+  game.scale.pageAlignHorizontally = true;
+  game.scale.pageAlignVertically = true;
+
+  game.stage.backgroundColor = '#2d2d2d';
+}
+/**
+ * create
+ */
+function create () {
+  // game.physics.startSystem(Phaser.Physics.ARCADE);
+  game.physics.startSystem(Phaser.Physics.P2JS);
+  game.physics.p2.setImpactEvents(true);
+
+  game.input.mouse.capture = true;
+}
+
+/**ß
+ * update
+ */
+function update () {
+}
