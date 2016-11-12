d9ead6a6114580f95646044e6d884f225f2c23da
Edmund Quintero <equintero@tradeking.com>
2016-11-11 21:31:54 -0500

    Player progress. not showing assets

--- src/game.js
+++ src/game.js
@@ -1,4 +1,5 @@
 import { World } from './world/world';
+import { Sprites } from './Sprites/Sprites'
 
 /**
  * Bootstraps the game and execute Phaser lifecycle hooks
@@ -14,18 +15,28 @@ function preload () {
   game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
   game.scale.pageAlignHorizontally = true;
   game.scale.pageAlignVertically = true;
+  // game.stage.backgroundColor = '#2d2d2d';
 
+<<<<<<< 0245e2262fe448d3eacf99bd9bd17c596171a95f
   game.stage.backgroundColor = '#2d2d2d';
 
   game.load.tilemap('level1', 'maps/Level1/map.json', null, Phaser.Tilemap.TILED_JSON);
 
   game.load.image('Tiles', 'images/tile_sprites.png');
+=======
+  game.playerLayer = game.add.group();
+
+  game.world.bringToTop(game.playerLayer);
+
+
+  new Sprites().load();
+>>>>>>> Player progress. not showing assets
 }
 /**
  * create
  */
 function create () {
-  // game.physics.startSystem(Phaser.Physics.ARCADE);
+
   game.physics.startSystem(Phaser.Physics.P2JS);
   game.physics.p2.setImpactEvents(true);
 
--- src/world/world.js
+++ src/world/world.js
@@ -1,9 +1,10 @@
 import { game } from '../game';
-
+import { Player } from '../Player/Player'
 
 export class World {
     constructor() {
         this.game = game;
         console.log('new World');
+        this.player = new Player();
     }
 }
