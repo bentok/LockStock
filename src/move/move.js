f0782e1ea15a239c250da019aa33d7efbf370057
Edmund Quintero <edmundquintero@gmail.com>
2016-11-12 10:44:08 -0500

    adjusts scale

--- src/move/move.js
+++ src/move/move.js
@@ -17,9 +17,6 @@ export class Move {
     this.character.direction = 'right';
   }
 
-  idle () {
-    // this.character.sprite.body.velocity.x = 0;
-    // this.character.sprite.body.velocity.y = 0;
-  }
+  idle () {}
 
 }
