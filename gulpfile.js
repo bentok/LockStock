a3be7076c0360bc8b3b380570cbc925a81b57730
Ben Copeland <bscopeland@gmail.com>
2016-11-11 19:25:47 -0500

    Adds gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+const gulp = require('gulp');
+const browserify = require('browserify');
+const source = require('vinyl-source-stream');
+
+gulp.task('default', ['build', 'watch']);
+gulp.task('build', ['copyImages', 'browserify']);
+
+gulp.task('watch', () => {
+  gulp.watch('src/**/*.js', ['browserify']);
+});
+
+// Compile less
+gulp.task('less', () => gulp.src('src/**/*.less')
+  .pipe(less())
+  .pipe(gulp.dest('public')));
+
+// Copy images to public
+gulp.task('copyImages', () => gulp.src('src/images/*.*')
+  .pipe(gulp.dest('public/images')));
+
+// Applies transforms to Javascript and bundles it
+gulp.task('browserify', () => {
+  return browserify({ 
+    entries: [
+      'src/game.js'
+    ]
+  })
+  .transform('babelify', {
+    'presets': ['es2015']
+  })
+  .bundle()
+  .pipe(source('main.bundle.js'))
+  .pipe(gulp.dest('pubic/js'));
+});
