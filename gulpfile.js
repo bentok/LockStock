2c4a7b33081d853604b1a3aeef035e77b15a519e
Ben Copeland <bscopeland@gmail.com>
2016-11-11 22:09:34 -0500

    Removes unneeded task

--- gulpfile.js
+++ gulpfile.js
@@ -9,11 +9,6 @@ gulp.task('watch', () => {
   gulp.watch('src/**/*.js', ['browserify']);
 });
 
-// Compile less
-gulp.task('less', () => gulp.src('src/**/*.less')
-  .pipe(less())
-  .pipe(gulp.dest('public')));
-
 // Copy images to public
 gulp.task('copyImages', () => gulp.src('src/images/*.*')
   .pipe(gulp.dest('public/images')));
