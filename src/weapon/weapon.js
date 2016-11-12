294db9533627235bb1b0da483da254cd38b89dec
Ben Copeland <bscopeland@gmail.com>
2016-11-12 15:08:37 -0500

    Avoids collision with own bullets on discharge

--- src/weapon/weapon.js
+++ src/weapon/weapon.js
@@ -9,8 +9,8 @@ export class Weapon extends Phaser.State {
     this.velocity = 400;
     this.rate = 60;
     this.spread = 10;
-    this.numPellets = 5;
-    this.damage = 20 * this.numPellets;
+    this.bulletsPerShot = 5;
+    this.damage = 20 * this.bulletsPerShot;
     this.capacity = 2;
     this.currentCapacity = this.capacity; //spawn with full clip
     this.nextFire = 0;
@@ -18,7 +18,6 @@ export class Weapon extends Phaser.State {
   }
 
   render () {
-
     this.game.projectilesLayer = this.game.add.group();
     this.game.projectilesLayer.enableBody = true;
     this.game.projectilesLayer.physicsBodyType = Phaser.Physics.P2;
@@ -29,13 +28,16 @@ export class Weapon extends Phaser.State {
   }
 
   fire () {
+    // Offset to avoid collision between player and their own bullets
+    const aimOffset = this.character.aimDirection === 'right' ? 15 : -15;
     if (this.game.time.now > this.nextFire && this.game.projectilesLayer.countDead() > 0) {
         this.nextFire = this.game.time.now + this.rate;
         let projectiles =  [];
-        for (let i = 0; i < this.numPellets; i++) {
+        for (let i = 0; i < this.bulletsPerShot; i++) {
           projectiles.push(this.game.projectilesLayer.getFirstDead());
-          projectiles[i].reset(this.character.sprite.x, this.character.sprite.y);
-          this.discharge(1); //number of shells fired per shot
+          projectiles[i].reset(this.character.sprite.x + aimOffset, this.character.sprite.y);
+          // Number of shells fired per shot
+          this.discharge(this.bulletsPerShot); 
           const dest = {
             projectile: projectiles[i],
             x: this.calculateTrajectory({ i, coordinate: this.character.reticle.body.x }),
